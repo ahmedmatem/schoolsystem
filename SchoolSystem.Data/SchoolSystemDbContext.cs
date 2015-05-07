c3bf88a42de19f445147220dcceaa6550e05ecc6
Ahmed Ahmed <ahmedmatem@gmail.com>
2015-05-07 22:41:24 +0100

    Create Model and Data class libraries. Create ASP MVC Web Application. Enabled Data Migration. Move ApplicationUser class from Web to Model as User class. Set SQL database in Web.config file with name SchoolSystemDB.

---
+++ SchoolSystem.Data/SchoolSystemDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace SchoolSystem.Data
+{
+    using System.Data.Entity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    using SchoolSystem.Model;
+
+    public class SchoolSystemDbContext : IdentityDbContext<User>
+    {
+        public SchoolSystemDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static SchoolSystemDbContext Create()
+        {
+            return new SchoolSystemDbContext();
+        }
+    }
+}
